7e15262421189cd060239b4f926bb47fb1f83cf2
xiaoxuan.jin <xiaoxuan.jin@qunar.com>
2017-02-14 19:20:11 +0800

    add

--- jasmine-web/src/test/java/com/tian/apply/pdf/PdfBoxTest.java
+++ jasmine-web/src/test/java/com/tian/apply/pdf/PdfBoxTest.java
@@ -27,9 +27,10 @@ public class PdfBoxTest {
     // 发票金额
     private static String invoiceAmountRegex = "(.*?[价][税][合][计].*?)(\\d+)(.*)";
     // 发票抬头
-    private static String invoiceTitleRegex = "(.*?[价][税][合][计].*?)(\\d+)(.*)";
+    private static String invoiceTitleRegex = "[校]\\s[验]\\s[码]:\\s(\\d+?\\s?)+?[名]\\s+?[称]:\\s+(.*)\\s+";
     // 开票地区
     private static String localBillingRegex = "(.*?\\s+?)([\\u4E00-\\u9FA5]+[增][值][税][电][子][普][通][发][票])(.*)";
+
     @Test
     public void testPdfBox() throws Exception {
 
@@ -42,21 +43,30 @@ public class PdfBoxTest {
 
             pdfTextStripper.processPage(firstPage);
             String text = pdfTextStripper.getText(document);
-
-            Pattern patter = Pattern.compile(localBillingRegex);
-            Matcher matcher = patter.matcher(text);
             System.out.println(text);
+            Pattern localBillingPattern = Pattern.compile(localBillingRegex);
+            Pattern invoiceTitleRegexPattern = Pattern.compile(invoiceTitleRegex);
             System.out.println("==========================\n\t\t\t结果\n=========================");
-            while (matcher.find()) {
-                System.out.println("开票地区：" + matcher.group(2));
+            find(invoiceTitleRegexPattern, text, "发票抬头", 4);
+            find(localBillingPattern, text, "开票地区", 2);
+
+            String mydata = "some string with 'the data i want' inside";
+            Pattern pattern = Pattern.compile("'(.*?)'");
+            Matcher matcher = pattern.matcher(mydata);
+            if (matcher.find())
+            {
+                System.out.println(matcher.group(1));
             }
-
-
-
-
 
         } finally {
         }
 
+    }
+
+    private void find(Pattern localBillingPattern, String text, String name, int group) {
+        Matcher matcher = localBillingPattern.matcher(text);
+        while (matcher.find()) {
+            System.out.println(name + "：" + matcher.group(group));
+        }
     }
 }
