3f025e7519bb2369b4c863601ee043f67307a7db
xiaoxuan.jin <xiaoxuan.jin@qunar.com>
2017-02-22 19:01:24 +0800

    解析电子发票

--- jasmine-web/src/test/java/com/tian/apply/pdf/PdfBoxTest.java
+++ jasmine-web/src/test/java/com/tian/apply/pdf/PdfBoxTest.java
@@ -21,39 +21,40 @@ import java.util.regex.Pattern;
  */
 public class PdfBoxTest {
     // 发票代码
-    private static String invoiceCodeRegex = "[发][票][代][码]:\\s*(\\d+)";
+    private static String invoiceCodeRegex = "发票代码:[\\s\\u3000]*(\\d+)";
     public static Pattern invoiceCodePattern = Pattern.compile(invoiceCodeRegex);
 
     // 发票号码
-    private static String invoiceNumberRegex = "[发][票][号][码]:\\s*(\\d+)";
+    private static String invoiceNumberRegex = "发票号码:[\\s\\u3000]*(\\d+)";
     public static Pattern invoiceNumberPattern = Pattern.compile(invoiceNumberRegex);
 
     // 开票日期
-    private static String invoiceDateRegex = "[开][票][日][期]:\\s*(\\d+)\\s*[年]\\s*(\\d+)\\s*[月]\\s*(\\d+)\\s*[日]";
+    private static String invoiceDateRegex = "开票日期:[\\s\\u3000]*(\\d+)[\\s\\u3000]*[年][\\s\\u3000]*(\\d+)[\\s\\u3000]*[月][\\s\\u3000]*(\\d+)[\\s\\u3000]*[日]";
     public static Pattern invoiceDatePattern = Pattern.compile(invoiceDateRegex);
 
     // 校验码
-    private static String invoiceCheckCodeRegex = "[校]\\s*[验]\\s*[码]:\\s*((\\d+\\s*)+)";
+    private static String invoiceCheckCodeRegex = "[校][\\s\\u3000]*[验][\\s\\u3000]*[码]:[\\s\\u3000]*((\\d+[\\s\\u3000]*)+)";
     public static Pattern invoiceCheckCodePattern = Pattern.compile(invoiceCheckCodeRegex);
 
     // 发票金额
-    private static String invoiceAmountRegex = "[价][税][合][计].*?(\\d+\\.?\\d+)";
+    private static String invoiceAmountRegex = "价税合计.*?(\\d+\\.?\\d+)";
     public static Pattern invoiceAmountPattern = Pattern.compile(invoiceAmountRegex);
 
     // 发票抬头
-    private static String invoiceTitleRegex = "[校]\\s*[验]\\s*[码]:\\s*(\\d+\\s*)+?[名]\\s*[称]:\\s*(\\S*)";
+    //"[校]\\s*[验]\\s*[码]:\\s*(\\d+\\s*)+?[名]\\s*[称]:\\s*(\\S*)";
+    private static String invoiceTitleRegex = "[校][\\s\\u3000]*[验][\\s\\u3000]*[码]:[\\s\\u3000]*(\\d+[\\s\\u3000]*)+?[名][\\s\\u3000]*[称]:[\\s\\u3000]*(\\S*)";
     public static Pattern invoiceTitlePattern = Pattern.compile(invoiceTitleRegex);
 
     // 销售方名称
-    private static String sellerNameRegex = "[价][税][合][计].*?\\s*[名]\\s*[称]:\\s*(\\S*)";
+    private static String sellerNameRegex = "价税合计.*?[\\s\\u3000]*[名][\\s\\u3000]*[称]:[\\s\\u3000]*(\\S*)";
     public static Pattern sellerNamePattern = Pattern.compile(sellerNameRegex);
 
     // 开票地区
-    private static String localBillingRegex = "(.*?\\s*)([\\u4E00-\\u9FA5]+[增][值][税][电][子][普][通][发][票])(.*)";
+    private static String localBillingRegex = "(.*?[\\s\\u3000]*)([\\u4E00-\\u9FA5]+增值]税电子普通发票)(.*)";
     public static Pattern localBillingPattern = Pattern.compile(localBillingRegex);
 
     // 开票内容
-    private static String invoiceContentRegex = "[税]\\s*[额]\\s*((\\S*)\\s*.*?\\s*)[合]";
+    private static String invoiceContentRegex = "[税][\\s\\u3000]*[额][\\s\\u3000]*((\\S*)[\\s\\u3000]*.*?[\\s\\u3000]*)[合]";
     public static Pattern invoiceContentPattern = Pattern.compile(invoiceContentRegex);
 
     @Test
@@ -71,7 +72,7 @@ public class PdfBoxTest {
 
     @Test
     public void testString() throws Exception {
-        String aaa= "我的动感地带";
+        String aaa = "我的动感地带";
         System.out.println(aaa.length());
         System.out.println(aaa.substring(0, 5));
     }
@@ -141,11 +142,31 @@ public class PdfBoxTest {
     }
 
 
-    private void findInvoiceTitle() {
+    @Test
+    public void findInvoiceTitle() {
+        String reg = "[名][\\s\\u3000]*?[称]";
+
+        String input1 = "名　　　　称: 北京趣拿软件科技有限公司";
         Matcher matcher = invoiceTitlePattern.matcher(text);
         while (matcher.find()) {
             System.out.println("发票抬头：" + matcher.group(2).trim());
         }
+
+
+    }
+
+    public static String stringToUnicode(String s) {
+        String str = "";
+        for (int i = 0; i < s.length(); i++) {
+            int ch = (int) s.charAt(i);
+            if (ch > 255)
+                str += s.charAt(i) + ": " + "\\u" + Integer.toHexString(ch)
+                        + "\n";
+            else
+                str += s.charAt(i) + ": " + "\\u00" + Integer.toHexString(ch)
+                        + "\n";
+        }
+        return str;
     }
 
 
@@ -161,7 +182,8 @@ public class PdfBoxTest {
     {
         PDDocument document = null;
         try {
-            document = PDDocument.load(new File("C:\\Users\\Administrator\\Desktop\\滴滴电子发票.pdf"));
+            // 1487301251998-滴滴电子发票
+            document = PDDocument.load(new File("C:\\Users\\Administrator\\Desktop\\1487301251998-滴滴电子发票.pdf"));
             //document = PDDocument.load(new File("C:\\Users\\Administrator\\Desktop\\D45467838784.pdf"));
             PDFTextStripper pdfTextStripper = new PDFTextStripper();
             pdfTextStripper.setSortByPosition(true);
@@ -171,19 +193,18 @@ public class PdfBoxTest {
             text = pdfTextStripper.getText(document);
             System.out.println(text);
 
-            System.out.println("==========================\n\t\t\t开票内容\n=========================");
+            System.out.println("==========================\n\t\t\t发票代码\n=========================");
 
             PDFTextStripperByArea pdfTextStripperByArea = new PDFTextStripperByArea();
             pdfTextStripperByArea.setSortByPosition(true);
             // Rectangle rect = new Rectangle(5, 170, 170, 99);
-            Rectangle rect = new Rectangle(5, 160, 170, 99);
+            Rectangle rect = new Rectangle(170, 50, 100, 10);
             pdfTextStripperByArea.addRegion("class1", rect);
             pdfTextStripperByArea.extractRegions(firstPage);
             System.out.println("Text in the area:" + rect);
             System.out.println(pdfTextStripperByArea.getTextForRegion("class1"));
 
             System.out.println("==========================\n\t\t\t结果\n=========================");
-
 
         } catch (Exception e) {
             e.printStackTrace();
