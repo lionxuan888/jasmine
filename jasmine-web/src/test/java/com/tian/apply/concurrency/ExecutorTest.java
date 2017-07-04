d197cc285b5901e7e88aa95a8fa833cc8b69f259
xiaoxuan.jin <xiaoxuan.jin@qunar.com>
2017-07-04 18:03:40 +0800

    post

--- jasmine-web/src/test/java/com/tian/apply/concurrency/ExecutorTest.java
+++ jasmine-web/src/test/java/com/tian/apply/concurrency/ExecutorTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.util.Comparator;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * Created by xiaoxuan.jin on 2017/6/22.
@@ -18,29 +19,57 @@ public class ExecutorTest {
 
     @Test
     public void testExecutor() throws Exception {
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
-        for (int i = 0; i < 10; i++) {
-            Future<String> submit = executorService.submit(new Callable<String>() {
-                public String call() throws Exception {
-                    Thread.sleep(1000);
-                    System.out.println(Thread.currentThread().getName() + "running...");
-                    return null;
+        Thread subThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                    System.out.println("thread is running...");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-            });
+
+            }
+        });
+        subThread.start();
+        Thread thread = Thread.currentThread();
+        while (thread.isAlive()) {
+            thread.wait();
         }
-        executorService.shutdown();
-        while(Thread.activeCount() > 1) {
-        }
+
         System.out.println("main thread exit...");
     }
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         LinkedBlockingQueue<String> blockingQueue = new LinkedBlockingQueue<String>(4);
-        for (int i = 0; i < 10; i++) {
-            String element = "test-" + i;
-            blockingQueue.put(element);
-            System.out.println("成功放入队列 + " + element);
-        }
+        ReentrantLock reentrantLock = new ReentrantLock();
+        Runnable target = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String threadName = Thread.currentThread().getName();
+                    System.out.println("entering " + threadName);
+                    reentrantLock.lock();
+                    System.out.println(threadName + "获取到分布式锁，开始运行程序");
+                    Thread.sleep(5000);
+                    System.out.println(threadName + " sub thread running end...");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    reentrantLock.unlock();
+                }
+
+            }
+        };
+        Thread subThread_1 = new Thread(target);
+
+        Thread subThread_2 = new Thread(target);
+        while(Thread.activeCount() > 1){}
+
+        System.out.println("主程序结束");
+
+        int TERMINATED =  3 << 29;
+        System.out.println(TERMINATED);
     }
 
 }
